6b38f313cd681b1add0b9e4205b8037a112785a7
lom <krivopolnik@gmail.com>
2024-07-30 17:45:30 +0200

    Initial commit

---
+++ src/main/java/com/borschevski/dataflow_app/DataFlowWebApplication.java
@@ -0,0 +1,13 @@
+package com.borschevski.dataflow_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFlowWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFlowWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/borschevski/dataflow_app/DataFlowWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borschevski.dataflow_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFlowWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
